2628b63205b0a02894c0ee1a3e9c12d857a10025
Christian Breyer <Christian.d.breyer@Outlook.com>
2019-08-06 16:49:27 -0500

    Initial Set up of game

---
+++ assets/game.js
@@ -0,0 +1,18 @@
+// setting values for gems
+
+//gem 1 = 1-10, guesses 1-5
+let gem1 = Math.floor(Math.random()*10) +1;
+let guess1 = Math.floor(Math.random()*6)+1;
+
+//gem 2 = 2-13, guesses 1-3
+let gem2 = Math.floor(Math.random() * 12) +1;
+let guess2 = Math.floor(Math.random() * 3) +1;
+
+//gem 3 = 3-16, guesses 1-2
+let gem3 = Math.floor(Math.random() * 16) +1;
+let guess3= Math.floor(Math.random() * 2) +1;
+
+//gem 4 = 1-5, guesses 1-8
+let gem4 = Math.floor(Math.Random() * 5) + 1;
+let guess4 = Math.floor(Math.random() * 8) + 1;
+

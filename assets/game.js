6e9895c4b142abda33bf21f99f3e2285628b26cc
Christian Breyer <Christian.d.breyer@Outlook.com>
2019-08-06 22:25:04 -0500

    Basic game Complete

--- assets/game.js
+++ assets/game.js
@@ -5,11 +5,11 @@ let gem1 = Math.floor(Math.random()*10) +1;
 let guess1 = Math.floor(Math.random()*6)+1;
 
 //gem 2 = 2-13, guesses 1-3
-let gem2 = Math.floor(Math.random() * 12) +1;
+let gem2 = Math.floor(Math.random() * 12) +2;
 let guess2 = Math.floor(Math.random() * 3) +1;
 
 //gem 3 = 3-16, guesses 1-2
-let gem3 = Math.floor(Math.random() * 16) +1;
+let gem3 = Math.floor(Math.random() * 16) +3;
 let guess3= Math.floor(Math.random() * 2) +1;
 
 //gem 4 = 1-5, guesses 1-8
@@ -26,7 +26,7 @@ document.getElementById("valueToGuess").innerHTML = correctAmount;
 let pGuessTotal =0;
 let gameRunning = true;
 let loseCounter = 0;
-let winCounter = 1;
+let winCounter = 0;
 
 for(i = 0; i < 4; i++){
     const crystalID = document.createElement("img");
@@ -34,9 +34,11 @@ for(i = 0; i < 4; i++){
     crystalID.setAttribute("src",gemImgArray[i]);
     crystalID.width = "150";
     crystalID.height="150";
+    crystalID.style.margin = "50px";
     crystalID.setAttribute("crystalValue", gemArray[i]);
     document.getElementById("crystals").append(crystalID);
 }
+
  document.querySelectorAll(".crystalClass").forEach(function (clicker){
     clicker.addEventListener("click", function() {
         if(gameRunning === true){
@@ -48,14 +50,35 @@ for(i = 0; i < 4; i++){
         if(pGuessTotal === correctAmount){
             winCounter++;
             gameRunning = false;
+            document.getElementById("wincounterID").innerHTML = winCounter;
+            document.getElementById("gamemention").innerHTML = "You Win! Press any Crystal to start New Game";
         }
         else if (pGuessTotal > correctAmount){
             loseCounter++;
             gameRunning = false;
+            document.getElementById("gamemention").innerHTML = "You lost, Click any Crystal to start a New Game";
         }
     }
     else{
-        // gameRunning = true;
+        pGuessTotal = 0;
+        document.getElementById("valueCurrentGuess").innerHTML = pGuessTotal;
+        gem1 = Math.floor(Math.random()*10) +1;
+        guess1 = Math.floor(Math.random()*6)+1;
+        gem2 = Math.floor(Math.random() * 12) +1;
+        guess2 = Math.floor(Math.random() * 3) +1;
+        gem3 = Math.floor(Math.random() * 16) +1;
+        guess3= Math.floor(Math.random() * 2) +1;
+        gem4 = Math.floor(Math.random() * 5) + 1;
+        guess4 = Math.floor(Math.random() * 8) + 1;
+        correctAmount = (gem1 * guess1) + (gem2 * guess2) + (gem3 * guess3) + (gem4 * gem4);
+        gemArray = [gem1,gem2,gem3,gem4];
+        document.getElementById("valueToGuess").innerHTML = correctAmount;
+        for(i = 0; i < 4; i++){
+            document.getElementsByClassName("crystalClass")[i].setAttribute("crystalvalue",gemArray[i]);
+        }
+        gameRunning = true;
+        document.getElementById("gamemention").innerHTML = "";
+        
     }
 })
  })
